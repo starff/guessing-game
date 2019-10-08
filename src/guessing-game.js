cefe5bda6229ae68767bca5ac5bca5aed5c826c5
starff <starff.st@gmail.com>
2019-10-09 00:34:31 +0300

    init: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
-class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+class GuessingGame { // все через this
+    constructor(first,second,target) {
+      this.first = first;
+      this.second = second;
+      this.target = target;
     }
-
+  
+    setRange(first, second) {
+      this.first = first;
+      this.second = second; 
+    }
+  
     guess() {
-
+      this.target = Math.round((this.first + this.second) / 2);
+      return this.target;
     }
-
+  
     lower() {
-
+      this.second = this.target - 1;
+    //   return this.second;
     }
-
+  
     greater() {
-
+      this.first = this.target + 1;
+    //   return this.first;
     }
-}
+  };
+  
+  module.exports = GuessingGame;
 
-module.exports = GuessingGame;
